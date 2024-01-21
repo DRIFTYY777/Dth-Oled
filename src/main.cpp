0650718b25fe66191a4e7845b189bc69a9dcd775
DRIFTYY777 <dhimandeepak517@gmail.com>
2024-01-21 21:50:29 +0530

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,102 @@
+#include <Arduino.h>
+#include <DHT.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_I2CDevice.h>
+
+#define DHT11PIN 23
+
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+DHT dht(DHT11PIN, DHT11);
+
+TaskHandle_t Core0Task;
+TaskHandle_t Core1Task;
+
+void readDHTValues(float &temp, float &humi)
+{
+  humi = dht.readHumidity();
+  temp = dht.readTemperature();
+}
+
+void printDHT11(void *pvParameters)
+{
+  for (;;)
+  {
+    float humi, temp;
+
+    readDHTValues(temp, humi);
+
+    Serial.print("Temperature: ");
+    Serial.print(temp);
+    Serial.print("ºC ");
+    Serial.print("Humidity: ");
+    Serial.println(humi);
+
+    display.clearDisplay();
+
+    display.setTextSize(1);
+    display.setCursor(0, 0);
+    display.print("HUMI: ");
+
+    display.setTextSize(2);
+    display.setCursor(0, 10);
+    display.print(humi);
+
+    display.setTextSize(1);
+    display.setCursor(0, 35);
+    display.print("TEMP: ");
+    display.setTextSize(2);
+    display.setCursor(0, 45);
+    display.print(temp);
+
+    display.display();
+
+    vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for 500 milliseconds
+  }
+}
+
+void Task2code(void *pvParameters)
+{
+  for (;;)
+  {
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  dht.begin();
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;
+  }
+  delay(2000);
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+}
+
+void loop()
+{
+  xTaskCreatePinnedToCore(
+      printDHT11, /* Task function. */
+      "Task1",    /* Name of the task. */
+      4096,       /* Stack size of the task. Adjust based on actual usage. */
+      NULL,       /* Parameter of the task. */
+      2,          /* Priority of the task. Higher value means higher priority. */
+      &Core0Task, /* Task handle to keep track of the created task. */
+      0);         /* Pin task to core 0. */
+
+  xTaskCreatePinnedToCore(
+      Task2code,  /* Task function. */
+      "Task2",    /* Name of the task. */
+      4096,       /* Stack size of the task. Adjust based on actual usage. */
+      NULL,       /* Parameter of the task. */
+      1,          /* Priority of the task. Lower value means lower priority. */
+      &Core1Task, /* Task handle to keep track of the created task. */
+      1);         /* Pin task to core 1. */
+
+      delay(1000);
+}
